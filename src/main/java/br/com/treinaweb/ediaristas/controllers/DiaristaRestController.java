94a238a46a00e7bf5946bfca0d512f8359ee70a4
Lucena <mariana.a.lucena@accenture.com>
2021-10-13 23:44:01 -0300

    Paginação no resultado de consulta diaristas

--- src/main/java/br/com/treinaweb/ediaristas/controllers/DiaristaRestController.java
+++ src/main/java/br/com/treinaweb/ediaristas/controllers/DiaristaRestController.java
@@ -1,14 +1,13 @@
 package br.com.treinaweb.ediaristas.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.treinaweb.ediaristas.models.Diarista;
+import br.com.treinaweb.ediaristas.dtos.DiaristasPagedResponse;
 import br.com.treinaweb.ediaristas.repositories.DiaristaRepository;
 import br.com.treinaweb.ediaristas.services.ViaCepService;
 
@@ -24,11 +23,18 @@ public class DiaristaRestController {
     private ViaCepService viaCepService;
 
     @GetMapping // = action, metodo mapeado atraves de uma rota
-    public List<Diarista> buscarDiaristasPorCep(@RequestParam String cep){
+    public DiaristasPagedResponse buscarDiaristasPorCep(@RequestParam String cep){
         var endereco = viaCepService.buscarEnderecoPorCep(cep);
         var codigoIbge = endereco.getIbge();
 
-        return repository.findByCodigoIbge(codigoIbge);
+        var pageable = PageRequest.of(0, 6); //PageRequest.of(numeroPagina, numeroElementosPorPagina); 0 = primeira pagina
+        var diaristas = repository.findByCodigoIbge(codigoIbge, pageable);
+
+        var quantidadeDiaristas = diaristas.getTotalElements() > 6 ? diaristas.getTotalElements() - 6 : 0; //quantidade que restam alem dos 6 que foram exibidos
+
+        return new DiaristasPagedResponse(diaristas.getContent(), quantidadeDiaristas);
+
+        //return repository.findByCodigoIbge(codigoIbge);
     }
     
 }
--- src/main/java/br/com/treinaweb/ediaristas/repositories/DiaristaRepository.java
+++ src/main/java/br/com/treinaweb/ediaristas/repositories/DiaristaRepository.java
@@ -1,7 +1,7 @@
 package br.com.treinaweb.ediaristas.repositories;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.treinaweb.ediaristas.models.Diarista;
@@ -10,6 +10,8 @@ public interface DiaristaRepository extends JpaRepository<Diarista, Long>{
 
     //é criada uma query com base no nome do metodo + retorno de metodo
     //findBy: busca a partir do CodigoIbge, funciona porque na classe Diarista ha um atributo com o mesmo nome codigoIbge. Retorna uma lista.
-    List<Diarista> findByCodigoIbge(String codigoIbge); 
+    Page<Diarista> findByCodigoIbge(String codigoIbge, Pageable pageable); //pageable: representa uma configuracao de paginacao
+
+    //List<Diarista> findByCodigoIbge(String codigoIbge);
     
 }

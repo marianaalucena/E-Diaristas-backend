70da2c716d1613ce56b88547ca1aa997947b23fd
Lucena <mariana.a.lucena@accenture.com>
2021-10-13 23:24:28 -0300

    Filtrar diaristas de acordo com o codigo IBGE

--- src/main/java/br/com/treinaweb/ediaristas/controllers/DiaristaRestController.java
+++ src/main/java/br/com/treinaweb/ediaristas/controllers/DiaristaRestController.java
@@ -5,21 +5,30 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.treinaweb.ediaristas.models.Diarista;
 import br.com.treinaweb.ediaristas.repositories.DiaristaRepository;
+import br.com.treinaweb.ediaristas.services.ViaCepService;
 
 @RestController //controller da API
 @RequestMapping("/api/diaristas-cidade")
 public class DiaristaRestController {
 
+    //injecao de dependencias
     @Autowired
     private DiaristaRepository repository;
 
+    @Autowired
+    private ViaCepService viaCepService;
+
     @GetMapping // = action, metodo mapeado atraves de uma rota
-    public List<Diarista> buscarDiaristasPorCep(){
-        return repository.findAll();
+    public List<Diarista> buscarDiaristasPorCep(@RequestParam String cep){
+        var endereco = viaCepService.buscarEnderecoPorCep(cep);
+        var codigoIbge = endereco.getIbge();
+
+        return repository.findByCodigoIbge(codigoIbge);
     }
     
 }
--- src/main/java/br/com/treinaweb/ediaristas/repositories/DiaristaRepository.java
+++ src/main/java/br/com/treinaweb/ediaristas/repositories/DiaristaRepository.java
@@ -1,9 +1,15 @@
 package br.com.treinaweb.ediaristas.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.treinaweb.ediaristas.models.Diarista;
 
 public interface DiaristaRepository extends JpaRepository<Diarista, Long>{
+
+    //é criada uma query com base no nome do metodo + retorno de metodo
+    //findBy: busca a partir do CodigoIbge, funciona porque na classe Diarista ha um atributo com o mesmo nome codigoIbge. Retorna uma lista.
+    List<Diarista> findByCodigoIbge(String codigoIbge); 
     
 }
